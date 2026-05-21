39acce3e42556d9acdbda14a4bf979b41d8ff9d7
Badara <bbabadara@gmail.com>
2026-05-21 12:26:20 +0000

    connexion

--- controller/authController.php
+++ controller/authController.php
@@ -5,7 +5,23 @@ $logout=function(){
 };
 
 $login=function(){
-loadView("auth/login");
+   $errors=[];
+    if (isset($_POST["connect"])) {
+        isEmpty("email",$_POST["email"],$errors,"Veuillez renseigner l'email");
+        isEmpty("password",$_POST["password"],$errors,"Veuillez renseigner le password");
+        
+        if (validate($errors)) {
+            $user=login($_POST["email"]);
+            // dd($user);
+            if ($user && $_POST["password"] == $user["mdp"]) {
+                $_SESSION["user"]=$user;
+                redirectTo("dashboard","index");
+            }else{
+                $errors["connect"]="email ou mot de passe incorrect";
+            }
+        }
+    }
+loadView("auth/login",["errors"=>$errors],"auth");
 };
 
 
@@ -13,9 +29,7 @@ loadView("auth/login");
 
 $actions=[
     "login"=>$login,
-    "logout"=>$logout,
-
-    
+    "logout"=>$logout, 
 ];
  $action=$_REQUEST["action"]??"login";
  
